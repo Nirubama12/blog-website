e596e7f2902ac1e01c392e1947a1b358740ddbaf
Nirubama12 <nirubama.anbu@yahoo.in>
2023-01-25 01:34:47 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 //creating express server
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
-app.use("/niru",(req,res)=>{
-    console.log("hey this is my url")
-})
+dotenv.config();
+
+app.use(express.json());
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(process.env.MONGO_URL, {})
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
+
 app.listen("5000", ()=>{
     console.log("Backend is running.")
-})
+});
